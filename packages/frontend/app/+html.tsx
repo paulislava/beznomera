f9b0b8b80a5bf843041565bb6651339fd04fc919
Paul Kondratov <paulislava@mail.ru>
2025-02-16 23:51:11 +0700

    Установил метрику

--- packages/frontend/app/+html.tsx
+++ packages/frontend/app/+html.tsx
@@ -15,6 +15,24 @@ export default function Root({ children }: { children: React.ReactNode }) {
 
         <title>{SITE_TITLE}</title>
 
+        <script
+          type='text/javascript'
+          dangerouslySetInnerHTML={{
+            __html: `(function(m,e,t,r,i,k,a){m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
+          m[i].l = 1 * new Date();
+          for(var j = 0; j<document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(99937060, "init", {
+          clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });`
+          }}
+        />
+
         {/*
           Disable body scrolling on web. This makes ScrollView components work closer to how they do on native.
           However, body scrolling is often nice to have for mobile web. If you want to enable it, remove this line.
@@ -23,7 +41,16 @@ export default function Root({ children }: { children: React.ReactNode }) {
 
         {/* Add any additional <head> elements that you want globally available on web... */}
       </head>
-      <body>{children}</body>
+      <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html:
+              '<div><img src="https://mc.yandex.ru/watch/99937060" style="position:absolute; left:-9999px;" alt="" /></div>'
+          }}
+        />
+
+        {children}
+      </body>
     </html>
   );
 }
