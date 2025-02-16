c3f72c51125a2808ed76c65a249ecbb77d1c9d65
Paul Kondratov <paulislava@mail.ru>
2025-02-16 18:02:51 +0700

    Без uuid

--- packages/backend/src/app/car/car.controller.ts
+++ packages/backend/src/app/car/car.controller.ts
@@ -2,7 +2,6 @@ import { Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import CAR_API, { CODE_PARAM, CarApi } from '@paulislava/shared/car/car.api';
 import { CarInfo, ShortCarInfo } from '@paulislava/shared/car/car.types';
 import { CarService } from './car.service';
-import { UUIDParam } from '../decorators/params.decorator';
 import { CurrentUser } from '../users/user.decorator';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
@@ -16,7 +15,7 @@ export class CarController implements CarApi {
   }
 
   @Post(CAR_API.backendRoutes.call)
-  call(@UUIDParam(CODE_PARAM) code: string): Promise<void> {
+  call(@Param(CODE_PARAM) code: string): Promise<void> {
     return this.carService.call(code);
   }
 
