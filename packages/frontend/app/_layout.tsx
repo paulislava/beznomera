b27ce6efc826ebe8794aa3405b142c269f5ff35a
Paul Kondratov <paulislava@mail.ru>
2025-02-17 17:35:06 +0700

    Убрал файл

--- packages/frontend/app/_layout.tsx
+++ packages/frontend/app/_layout.tsx
@@ -68,20 +68,22 @@ function RootLayoutNav() {
   return (
     <HelmetProvider context={helmetContext}>
       <Helmet>
-        <script async defer type='text/javascript'>{`(function (m, e, t, r, i, k, a) {
-      m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
-      m[i].l = 1 * new Date();
-      for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
-      k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
-    })
-      (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+        <script
+          type='text/javascript'
+          async
+          defer
+        >{`(function(m,e,t,r,i,k,a){m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
+          m[i].l = 1 * new Date();
+          for(var j = 0; j<document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-    ym(99937060, "init", {
-      clickmap: true,
-      trackLinks: true,
-      accurateTrackBounce: true,
-      webvisor: true
-    });`}</script>
+        ym(99937060, "init", {
+          clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });`}</script>
       </Helmet>
       <Container>
         <StyledSvg width={0} height={0}>
