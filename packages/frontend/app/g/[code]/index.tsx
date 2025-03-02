64b53ab9754770caa2db239e37f486df368252a8
Paul Kondratov <paulislava@mail.ru>
2025-03-03 02:06:17 +0500

    Задан alt

--- packages/frontend/app/g/[code]/index.tsx
+++ packages/frontend/app/g/[code]/index.tsx
@@ -159,7 +159,11 @@ const CallUserPage = () => {
 
           {/* {info.no && <CarNumber>{info.no}</CarNumber>} */}
           {info.imageUrl ? (
-            <CarExternalImage $aspectRatio={info.imageRatio} source={{ uri: info.imageUrl }} />
+            <CarExternalImage
+              alt={`${info.no}, ${info.brandRaw || info.brand?.title} ${info.model}`}
+              $aspectRatio={info.imageRatio}
+              source={{ uri: info.imageUrl }}
+            />
           ) : (
             <StyledCarImage color={info.color?.value ?? info.rawColor} />
           )}
