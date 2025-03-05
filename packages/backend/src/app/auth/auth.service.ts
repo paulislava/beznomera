3d395ddfd684309d0f5cc2acadf32637076aea55
Paul Kondratov <paulislava@mail.ru>
2025-03-05 05:03:37 +0500

    Без ts expect error

--- packages/backend/src/app/auth/auth.service.ts
+++ packages/backend/src/app/auth/auth.service.ts
@@ -117,7 +117,6 @@ export class AuthService {
     const user = await this.userRepository.save(
       this.userRepository.create({
         firstName: data.first_name,
-        // @ts-expect-error no type
         lastName: data.last_name,
         nickname: data.username,
         telegramID: data.id,
