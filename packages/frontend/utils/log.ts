812e8c9c79115397fba91d0bd31d29e172abe386
Paul Kondratov <paulislava@mail.ru>
2025-03-09 05:41:26 +0500

    Фикс лога

--- packages/frontend/utils/log.ts
+++ packages/frontend/utils/log.ts
@@ -2,5 +2,9 @@ const counterId = 99937060;
 
 export const handleEvent = (event: string, userParams?: Record<string, any>) => {
   console.log(`Event goal: ${event}`);
-  ym(counterId, 'reachGoal', event, { defer: true, userParams });
+  try {
+    ym(counterId, 'reachGoal', event, { defer: true, userParams });
+  } catch (error) {
+    console.error(error);
+  }
 };
