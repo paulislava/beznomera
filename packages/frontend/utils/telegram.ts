66c6e3101df1dfb07563db353ce499e9a0245cef
Paul Kondratov <paulislava@mail.ru>
2025-03-10 17:51:58 +0500

    Тест init

--- packages/frontend/utils/telegram.ts
+++ packages/frontend/utils/telegram.ts
@@ -1,10 +1,12 @@
-import { init, miniApp, miniAppReady } from '@telegram-apps/sdk-react';
+import { init, miniApp, initData, miniAppReady } from '@telegram-apps/sdk-react';
 
 export let isTelegramWebApp = false;
 
 export const initWebApp = () => {
   try {
     init();
+    miniApp.mount();
+    initData.restore();
     miniAppReady();
     miniApp.ready();
     isTelegramWebApp = true;
