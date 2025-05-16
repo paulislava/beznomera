a017a4021f391b77df5180d3de163ed814b7aab6
Paul Kondratov <paulislava@mail.ru>
2025-05-16 20:06:11 +0500

    Конфиг

--- packages/web/eslint.config.mjs
+++ packages/web/eslint.config.mjs
@@ -1,21 +1,21 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: __dirname
 });
 
 const eslintConfig = [
   {
-    ignores: ["node_modules", ".next"],
+    ignores: ['node_modules', '.next']
   },
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  eslintPluginPrettierRecommended,
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  eslintPluginPrettierRecommended
 ];
 
 export default eslintConfig;
