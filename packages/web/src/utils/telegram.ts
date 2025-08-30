f65cf3c101d3bc3fa7553513adb3e94f9dfa5a8c
Paul Kondratov <paulislava@mail.ru>
2025-08-30 13:59:51 +0500

    Тест

--- packages/web/src/utils/telegram.ts
+++ packages/web/src/utils/telegram.ts
@@ -12,12 +12,18 @@ export let isTelegramWebApp = false;
 export const initWebApp = async () => {
   try {
     init();
+    alert('init');
     miniApp.mount();
+    alert('mount');
     miniApp.ready();
+    alert('ready');
     initData.restore();
+    alert('restore');
     isTelegramWebApp = true;
     expandViewport();
+    alert('expandViewport');
     await requestFullscreen();
+    alert('fullscreen');
   } catch (error) {
     console.error(error);
   }
