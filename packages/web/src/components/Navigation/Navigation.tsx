b38a0e0598d8aec851d52b94b68a50c16c3a4f12
Paul Kondratov <paulislava@mail.ru>
2025-12-30 00:53:49 +0500

    Отступы

--- packages/web/src/components/Navigation/Navigation.tsx
+++ packages/web/src/components/Navigation/Navigation.tsx
@@ -14,7 +14,7 @@ import {
   Button
 } from '@heroui/react';
 import styled from 'styled-components';
-import { PageContainer, safeAreaInsetTop } from '@/ui/Styled';
+import { PageContainer } from '@/ui/Styled';
 import { isTelegramWebApp } from '@/utils/telegram';
 import { qrScanner } from '@telegram-apps/sdk-react';
 import qrCodeSvg from '@/assets/images/qrcode.svg';
@@ -33,7 +33,7 @@ const StyledNavbar = styled(Navbar)`
 `;
 
 const TgSpace = styled.div`
-  padding-top: calc(${safeAreaInsetTop()} + 20px);
+  /* padding-top: 60px; */
 `;
 
 const QRCode = styled(qrCodeSvg)`
--- packages/web/src/components/ProvidersContainer/ProvidersContainer.tsx
+++ packages/web/src/components/ProvidersContainer/ProvidersContainer.tsx
@@ -5,21 +5,20 @@ import { HeroUIProvider, ToastProvider } from '@heroui/react';
 import { ThemeProvider } from '@/context/Theme/Theme.context';
 import { useThemeSync } from '@/hooks/useTheme';
 import styled from 'styled-components';
-import { safeAreaInsetTop } from '@/ui/Styled';
+import { isTelegramWebApp } from '@/utils/telegram';
 
-const StyledToastProvider = styled(ToastProvider)`
-  padding-top: ${safeAreaInsetTop()};
+const Container = styled(HeroUIProvider)`
+  padding-top: ${isTelegramWebApp ? '60px' : 0};
 `;
 
 export const ProvidersContainer = ({ children }: { children: React.ReactNode }) => {
   useThemeSync();
-
   return (
     <ThemeProvider>
-      <HeroUIProvider>
-        <StyledToastProvider placement='top-center' />
+      <Container>
+        <ToastProvider placement='top-center' />
         {children}
-      </HeroUIProvider>
+      </Container>
     </ThemeProvider>
   );
 };
